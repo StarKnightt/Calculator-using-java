ef02aaf1421db3d1fa758e07cf8b4b1fffc17cf3
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-09 09:48:44 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -15,6 +15,7 @@ public class Calculator implements ActionListener{
 	Font myFont = new Font("Ink Free",Font.BOLD,30);
 	double num1=0,num2=0,result=0;
 	char operator;
+	
 	Calculator(){
 		
 		frame = new JFrame("Calculator");
